60ab325587e18a414ca6367532afaab7df89f79c
Charlie Castillo <djcharliecast@gmail.com>
2021-05-12 18:43:59 -0300

    entrega clase 3

---
+++ src/components/NavBar.js
@@ -0,0 +1,36 @@
+import { AppBar, Toolbar, IconButton, Typography, Button } from "@material-ui/core";
+import MenuIcon from '@material-ui/icons/Menu';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+export default function ButtonAppBar() {
+const classes = useStyles();
+
+    return (
+        <AppBar position="static" style={{background: "#e6b960"}}>
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h5" className={classes.title}>
+            CookApp
+          </Typography>
+          <Button color="inherit">Categoría 1</Button>
+          <Button color="inherit">Categoría 2</Button>
+          <Button color="inherit">Categoría 3</Button>
+          <Button color="inherit">Categoría 4</Button>
+        </Toolbar>
+      </AppBar>
+    )
+};
